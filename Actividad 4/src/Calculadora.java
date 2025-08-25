838eb6b11a01cfcf0119a2620b0a929ce20f0e58
JimH0202 <odemhurtarte@gmail.com>
2025-08-25 09:49:22 -0600

    [ADD] este es mi commit actividad 4

---
+++ Actividad 4/src/Calculadora.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String opcion; 
+        
+        do {
+            System.out.println("\n******* CALCULADORA *******");
+            System.out.println("1. Suma ('uno')");
+            System.out.println("2. Resta ('dos')");
+            System.out.println("3. Multiplicación ('tres')");
+            System.out.println("4. División ('cuatro')");
+            System.out.println("5. Potencia ('cinco')");
+            System.out.println("6. Raíz ('seis')");
+            System.out.println("7. Salir ('siete')");
+            System.out.print("\nElige una opción: ");
+            
+            opcion = sc.nextLine().toLowerCase(); // leer la opción como string y "para cambiar a int opcion para entrada de números: int opcion = Integer.parseInt(sc.nextLine()); // leer como número
+
+            
+            if (!opcion.equals("siete")) { //} while (opcion != 7);
+
+                try {
+                    System.out.print("Ingresa el valor de a: ");
+                    double a = Double.parseDouble(sc.nextLine());
+                    System.out.print("Ingresa el valor de b: ");
+                    double b = Double.parseDouble(sc.nextLine());
+
+                    switch (opcion) {
+                        case "uno": // Suma            //case 1: 
+                            System.out.println("Resultado: " + (a + b));
+                            break;
+                        case "dos": // Resta
+                            System.out.println("Resultado: " + (a - b));
+                            break;
+                        case "tres": // Multiplicación
+                            System.out.println("Resultado: " + (a * b));
+                            break;
+                        case "cuatro": // División
+                            if (b != 0) {
+                                System.out.println("Resultado: " + (a / b));
+                            } else {
+                                System.out.println("Error: División entre cero no permitida.");
+                            }
+                            break;
+                        case "cinco": // Potencia
+                            System.out.println("Resultado: " + Math.pow(a, b));
+                            break;
+                        case "seis": // Raíz
+                            if (b == 0) {
+                                System.out.println("Error: No se puede calcular raíz de índice 0.");
+                            } else {
+                                double resultado = Math.pow(a, 1.0 / b);
+                                System.out.println("Resultado: " + resultado);
+                            }
+                            break;
+                        default:
+                            System.out.println("Opción no válida. al seleccionar la acción coloca el número por su nombre.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: Debes ingresar números válidos.");
+                }
+            }
+
+        } while (!opcion.equals("siete"));
+        
+        System.out.println("Cerrando calculadora.......");
+        sc.close();
+    }
+
+    @Override
+    public String toString() {
+        return "Calculadora []";
+    }
+}
---
+++ FACTORIAL/SalidaAlgoritmo_202303768.java
@@ -0,0 +1,30 @@
+/**
+  * NAME:
+  * DATE:
+  * FILE:
+  * COMMENTS:
+  */
+
+public class f_202303768 extends eecs.Gui
+{
+   public static void main(String[] args)
+   {
+      // declare variables
+      String raptor_prompt_variable_zzyz = null;
+      ?? i = ??;
+      ?? n = ??;
+      ?? factorial = ??;
+      
+      raptor_prompt_variable_zzyz = "Coloque un número";
+      n = get???(raptor_prompt_variable_zzyz);
+      factorial = 1;
+      i = 1;
+      while	(1)
+      {
+         i = i + 1;
+         factorial = factorial * i;
+         if (n == i)	break;
+      }
+      printLine("El factorial de " + n + " es: " + factorial);
+   } // close main
+} // close f_202303768
